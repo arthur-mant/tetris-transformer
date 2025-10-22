f8f2f35163ad5cbb1734dd2d40e15be4d278c159
Arthur Martinelli Antonietto <arthur@liamf.c3sl.ufpr.br>
2025-10-22 14:07:08 -0300

    modificado penalidade

--- rewards.py
+++ rewards.py
@@ -10,7 +10,7 @@ class Rewards:
     def reward(self, l):
         return pow(l, self.rew_exp)/pow(4, self.rew_exp)
     def penalty(self, h):
-        return self.pen_multiplier*pow(20-h, self.pen_exp)/pow(22, self.pen_exp)
+        return self.pen_multiplier*pow(18-h, self.pen_exp)/pow(20, self.pen_exp)
     def penalty_gameover(self):
         #penaliza 1 tetris quando perde
         return -1
